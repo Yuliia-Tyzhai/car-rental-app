108936de77c848289d8e8e3d0120625cb1d63fa4
Yuliia <yuliadzyoba@gmail.com>
2025-02-27 02:51:24 +0200

    add filters selectors

---
+++ src/redux/favouriteCars/selectors.js
@@ -0,0 +1,1 @@
+export const selectFavouriteCars = state => state.favouriteCars;
---
+++ src/redux/filters/selectors.js
@@ -0,0 +1,3 @@
+export const selectBrandFilter = state => state.filters.brand;
+export const selectPriceFilter = state => state.filters.price;
+export const selectMileageFilter = state => state.filters.mileage;
